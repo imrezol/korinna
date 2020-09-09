5327afba1796663b02b729c237dc02d6aa90ffa2
Zoltan Imre <ImreZol@gmail.com>
2020-09-09 07:57:12 +0200

    NLP language detection example with fasttext - comments

--- NLP/language_identification/fasttext_exmaple.py
+++ NLP/language_identification/fasttext_exmaple.py
@@ -1,15 +1,18 @@
 # https://fasttext.cc/docs/en/language-identification.html
 # wget -O lid.176.bin https://dl.fbaipublicfiles.com/fasttext/supervised-models/lid.176.bin
 
+# third party libraries, dependencies
 import fasttext
 import bs4
 import urllib.request
 
 PRETRAINED_MODEL_PATH = './lid.176.bin'
 model = fasttext.load_model(PRETRAINED_MODEL_PATH)
+# scope
 k =3
 threshold = 0.1
 
+# dictionary
 languages =	{
   "__label__en": "English",
   "__label__hu": "Hungarian",
@@ -17,17 +20,19 @@ languages =	{
 
 
 def get_language_name(langId):
-    language = languages.get(langId)
-    if language == None:
+    language = languages.get(langId) # scope, dictionary
+    if language == None: # Null value
         return langId
     else:
         return language
 
 def print_predictions(title, predictions):
     print(title+" preditions:")
-    languages = predictions[0]
+    # array
+    languages = predictions[0]  #scope
     scores = predictions[1]
     for i in range(len(languages)):
+        # variable types, conversion
         print(str(i + 1)+".", get_language_name(languages[i]), ' language probability:',scores[i])
     print()
 
